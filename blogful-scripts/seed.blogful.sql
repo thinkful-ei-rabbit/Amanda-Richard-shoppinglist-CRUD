df3e68fad677d599f22782752c570774d29b41b3
unknown <richardcarmenscott@gmail.com>
2020-09-23 11:38:27 -0400

    initial

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,11 @@
+insert into blogful_articles (title, date_published, content)
+VALUES
+    ('What to do?',  now() - '11 days'::INTERVAL, 'Lorem Spesum Mmooe Ffaahah'),
+    ('Where to go?',  now() - '12 days'::INTERVAL, 'Lorem Spesum Mmooe Ffaahah'),
+    ('What to say?',  now() - '13 days'::INTERVAL, 'Lorem Spesum Mmooe Ffaahah'),
+    ('How to do it?',  now() - '14 days'::INTERVAL, 'Lorem Spesum Mmooe Ffaahah'),
+    ('Is it to do?',  now() - '15 days'::INTERVAL, 'Lorem Spesum Mmooe Ffaahah'),
+    ('Can what I do help?',  now() - '21 days'::INTERVAL, 'Lorem Spesum Mmooe Ffaahah'),
+    ('No way to do it?',  now() - '22 days'::INTERVAL, 'Lorem Spesum Mmooe Ffaahah'),
+    ('Share?',  now() - '23 days'::INTERVAL, 'Lorem Spesum Mmooe Ffaahah'),
+    ('Live?',  now() - '24 days'::INTERVAL, 'Lorem Spesum Mmooe Ffaahah');
---
+++ src/drills.js
@@ -0,0 +1,63 @@
+require('dotenv').config();
+const knex = require('knex');
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL,
+});
+
+const string = 'seitan';
+const pageNumber = 1;
+const daysAgo = '11 days';
+
+function searchTerm(search) {
+  return knexInstance('shopping_list')
+    .select('*')
+    .where('name', 'ilike', `%${search}%`)
+    .then(results => {
+        console.log(results)
+        knexInstance.destroy();
+    })
+    
+}
+
+//searchTerm(string);
+
+function pagination(pageNumber) {
+    let offset = (pageNumber - 1) * 6;
+
+    return knexInstance('shopping_list')
+    .limit(6)
+    .offset(offset)
+    .then(result => {
+        console.log(result)
+        knexInstance.destroy();
+    })
+}
+
+//pagination(pageNumber);
+
+function findTime(daysAgo) {
+    return knexInstance('shopping_list')
+    .select('*')
+    .where('date_added', '>', knexInstance.raw(`now() - '?? days'::INTERVAL`, daysAgo))
+    .then(result => {
+        console.log(result)
+        knexInstance.destroy();
+    })
+}
+
+//findTime(daysAgo)
+
+function totals() {
+    return knexInstance('shopping_list')
+    .select('category')
+    .sum('price')
+    .groupBy('category')
+    .then(result => {
+        console.log(result)
+        knexInstance.destroy();
+    })
+}
+
+//totals()
